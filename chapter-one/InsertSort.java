09b73ee18185fef798a7d76f17a18c25bfe5bb25
mujin <chenjingran@163.com>
2015-08-30 23:17:41 +0800

    The insertsort demo

---
+++ chapter-one/InsertSort.java
@@ -0,0 +1,30 @@
+/**
+  * @Date: 2015-08-30
+  * @Author: chenjingran
+  * @Description: The first example in Introduce to Alogrithm
+  */
+
+public class InsertSort{
+	public static void main(String[] args){
+		int[] num = {3,1,34,67,89,43,23,124};
+		InsertSort insertSort = new InsertSort();
+		insertSort.insertSort(num);
+		for(int i = 0; i < num.length; i++)
+			System.out.println(num[i]);
+
+	}
+
+	public int insertSort(int[] num){
+		int i,j,temp;
+		for(i = 1; i <  num.length; i++){
+			temp = num[i];
+			j = i - 1;
+			while(j >= 0 && num[j] > temp){
+				num[j+1] = num[j];
+				j--;
+			}
+            num[j+1] = temp;
+		}
+		return 0;
+	}
+}
